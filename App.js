d262b9dfa9ddd173459b4b2d4dfec26cb2de749a
creativerze <creativerze@gmail.com>
2024-05-14 11:35:44 +0600

    added two sections

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, Image, ImageBackground, StyleSheet, Text, View } from 'react-native';
 import { useFonts, Inter_400Regular, Inter_700Bold } from '@expo-google-fonts/inter';
 
 
@@ -14,19 +14,82 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={{ fontFamily: "inter-regular" }}>My New App</Text>
-      <Text style={{ fontFamily: "inter-bold" }}>My New App</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground source={require("./assets/light-bg.png")}
+      style={{ flex: 1 }}>
+
+
+      <View style={styles.container}>
+
+        <View style={styles.topSection}>
+          <View style={styles.topSectionLeft}>
+            <Text style={styles.topSectionTextOne}>
+              “The science of operations, as derived from mathematics more especially, is a science of itself, and has its own abstract truth and value.”
+            </Text>
+            <Text style={styles.topSectionTextTwo}>
+              - Ada Lovelace
+            </Text>
+          </View>
+          <Image source={require("./assets/refresh.png")} />
+
+        </View>
+
+        <View style={{ marginBottom: 36 }}>
+
+          <View style={{ flexDirection: "row", alignItems: "center" }}>
+            <Image source={require("./assets/sun.png")} />
+            <Text style={{ fontFamily: "inter-regular", fontSize: 15, color: "white", marginLeft: 8, letterSpacing: 5 }}>
+              GOOD MORNING
+            </Text>
+          </View>
+
+          <View style={{ marginTop: 8 }}>
+            <Text>
+              <Text style={{ fontFamily: "inter-bold", fontSize: 100, color: "white" }}>11:30</Text>
+              <Text style={{ fontFamily: "inter-regular", fontSize: 15, color: "white" }}>
+                BST
+              </Text>
+            </Text>
+          </View>
+
+          <View style={{ marginTop: 16 }}>
+
+
+            <Text style={{ fontFamily: "inter-bold", fontSize: 15, color: "white", letterSpacing: 3 }}>
+              IN LONDON, UK
+            </Text>
+
+          </View>
+
+        </View>
+
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+    marginTop: 32,
+    paddingHorizontal: 26
   },
+  topSection: {
+    flexDirection: "row",
+
+  },
+  topSectionLeft: {
+    flex: 1,
+  },
+  topSectionTextOne: {
+    fontFamily: "inter-regular",
+    color: "white",
+    fontSize: 12,
+  },
+  topSectionTextTwo: {
+    fontFamily: "inter-bold",
+    color: "white",
+    fontSize: 12,
+    marginTop: 8,
+  }
 });
