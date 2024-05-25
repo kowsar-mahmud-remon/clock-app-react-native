de42734bf83a59ecce64b8b79f7a00f2b4894e2f
creativerze <creativerze@gmail.com>
2024-05-25 11:24:10 +0600

    update code

--- App.js
+++ App.js
@@ -151,84 +151,201 @@
 
 ////////////////////Stack Navigation/////////////
 
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import { Text, View, TouchableOpacity, Image } from 'react-native';
+
+
+// function HomeScreen({ navigation }) {
+//   const userId = 1;
+//   return (
+
+//     <View>
+//       <Text>Home screen</Text>
+
+//       <TouchableOpacity onPress={() => {
+//         navigation.navigate("Detail", { userId });
+//       }}>
+//         <Text>Go to detail</Text>
+//       </TouchableOpacity>
+//     </View>
+//   );
+// };
+
+// function DetailsScreen({ navigation, route }) {
+//   const id = route.params.userId;
+//   return (
+//     <View>
+//       <Text>Details Screen</Text>
+//       <Text>{id}</Text>
+//     </View>
+//   );
+// };
+
+// const Stack = createNativeStackNavigator();
+
+
+// const Logo = () => {
+//   return (
+//     <Image source={require("./assets/favicon.png")} />
+//   );
+// };
+
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator
+//         initialRouteName="Home"
+//         screenOptions={{
+//           headerStyle: {
+//             backgroundColor: "orange",
+//           },
+//           headerTintColor: "white",
+//           headerTitleStyle: {
+//             fontWeight: "bold",
+//             fontSize: 30
+//           }
+//         }}
+//       >
+//         <Stack.Screen
+//           name="Home"
+//           component={HomeScreen}
+//         />
+//         <Stack.Screen
+//           name="Detail"
+//           component={DetailsScreen}
+//           options={{
+//             title: "Detail Page",
+//             // headerTitle: () => <Logo />,
+//             headerStyle: {
+//               backgroundColor: "blue",
+//             },
+//             headerTintColor: "white",
+//             headerTitleStyle: {
+//               fontWeight: "bold",
+//               fontSize: 30
+//             }
+//           }}
+//         />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
+
+
+
+////////////////////Bottom tab navigation/////////////
+
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+// import { Text, View, TouchableOpacity, Image } from 'react-native';
+
+// const Tab = createBottomTabNavigator();
+
+
+// function HomeScreen() {
+//   return (
+
+//     <View>
+//       <Text>Home screen</Text>
+//     </View>
+//   );
+// };
+
+// function DetailsScreen() {
+//   return (
+//     <View>
+//       <Text>Details Screen</Text>
+//     </View>
+//   );
+// };
+
+// function ProfileScreen() {
+//   return (
+//     <View>
+//       <Text>Profile Screen</Text>
+//     </View>
+//   );
+// };
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <Tab.Navigator
+//         initialRouteName="Home"
+//       >
+//         <Tab.Screen
+//           name="Home"
+//           component={HomeScreen}
+//         />
+//         <Tab.Screen
+//           name="Detail"
+//           component={DetailsScreen}
+//         />
+//         <Tab.Screen
+//           name="Profile"
+//           component={ProfileScreen}
+//         />
+//       </Tab.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
+////////////////////Drawer navigation/////////////
+
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { Text, View, TouchableOpacity, Image } from 'react-native';
 
+const Drawer = createDrawerNavigator();
 
-function HomeScreen({ navigation }) {
-  const userId = 1;
+
+function HomeScreen() {
   return (
 
     <View>
       <Text>Home screen</Text>
-
-      <TouchableOpacity onPress={() => {
-        navigation.navigate("Detail", { userId });
-      }}>
-        <Text>Go to detail</Text>
-      </TouchableOpacity>
     </View>
   );
 };
 
-function DetailsScreen({ navigation, route }) {
-  const id = route.params.userId;
+function DetailsScreen() {
   return (
     <View>
       <Text>Details Screen</Text>
-      <Text>{id}</Text>
     </View>
   );
 };
 
-const Stack = createNativeStackNavigator();
-
-
-const Logo = () => {
+function ProfileScreen() {
   return (
-    <Image source={require("./assets/favicon.png")} />
+    <View>
+      <Text>Profile Screen</Text>
+    </View>
   );
 };
-
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
+      <Drawer.Navigator
         initialRouteName="Home"
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: "orange",
-          },
-          headerTintColor: "white",
-          headerTitleStyle: {
-            fontWeight: "bold",
-            fontSize: 30
-          }
-        }}
       >
-        <Stack.Screen
+        <Drawer.Screen
           name="Home"
           component={HomeScreen}
         />
-        <Stack.Screen
+        <Drawer.Screen
           name="Detail"
           component={DetailsScreen}
-          options={{
-            title: "Detail Page",
-            // headerTitle: () => <Logo />,
-            headerStyle: {
-              backgroundColor: "blue",
-            },
-            headerTintColor: "white",
-            headerTitleStyle: {
-              fontWeight: "bold",
-              fontSize: 30
-            }
-          }}
         />
-      </Stack.Navigator>
+        <Drawer.Screen
+          name="Profile"
+          component={ProfileScreen}
+        />
+      </Drawer.Navigator>
     </NavigationContainer>
   );
 }
